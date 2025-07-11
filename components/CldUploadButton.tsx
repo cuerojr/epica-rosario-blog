c42fc82745cfc7565a4f05fe399621c0be6a7865
cuerojr <cannibal5033@gmail.com>
2025-07-11 09:33:46 -0300

    wip started implementation

--- components/CldUploadButton.tsx
+++ components/CldUploadButton.tsx
@@ -1,47 +1,57 @@
 "use client";
-// CldUploadButton.tsx
 import { CldUploadWidget } from "next-cloudinary";
 import { toast } from "./ui/use-toast";
+import { useRef } from "react";
 
 export default function ImageUploadButton({
   onUpload,
 }: {
-  onUpload: (url: string) => void;
+  onUpload: (urls: string[]) => void;
 }) {
+  const uploadedUrlsRef = useRef<string[]>([]);
+
   return (
     <CldUploadWidget
       signatureEndpoint="/api/sign-image"
       options={{
         sources: ["local", "camera", "facebook", "instagram"],
-        multiple: false,
-        maxFileSize: 2 * 1024 * 1024, // ✅ máximo 2MB
-        clientAllowedFormats: ["jpg", "png", "jpeg", "webp"], // ✅ tipos permitidos
-        cropping: false, // o true si querés permitir recorte
+        multiple: true,
+        maxFileSize: 2 * 1024 * 1024,
+        clientAllowedFormats: ["jpg", "png", "jpeg", "webp"],
+        //cropping: true,
+        maxFiles: 5,
       }}
-      onSuccess={(result: any, widget: any) => {
-        const url = result?.info?.secure_url;
-
+      onSuccess={(result: any, widget: any) => {        
         if (result.event === "success") {
           const info = result.info;
           const width = info.width;
           const height = info.height;
+          const url = info.secure_url;
 
-          // ✅ Validar dimensiones (ej: entre 400x400 y 2000x2000)
           if (width < 400 || height < 400 || width > 2000 || height > 2000) {
-            // Opcional: borrar imagen del widget si no cumple
             toast({
               title: "Error",
-              description: "La imagen debe tener dimensiones entre 400x400 y 2000x2000 píxeles.",
+              description:
+                "La imagen debe tener dimensiones entre 400x400 y 2000x2000 píxeles.",
               variant: "destructive",
             });
-            widget.close();
             return;
           }
 
           if (url) {
-            onUpload(url);
+            uploadedUrlsRef.current.push(url);
           }
         }
+
+        console.log(result)
+        // Cuando termina la cola de subidas
+        //if (result.event === "queues-end") {
+          if (uploadedUrlsRef.current.length > 0) {
+            onUpload(uploadedUrlsRef.current);
+            uploadedUrlsRef.current = [];
+          }
+          widget.close();
+        //}
       }}
     >
       {({ open }) => (
@@ -50,7 +60,7 @@ export default function ImageUploadButton({
           type="button"
           onClick={() => open()}
         >
-          Cargar imagen
+          Cargar imágenes
         </button>
       )}
     </CldUploadWidget>

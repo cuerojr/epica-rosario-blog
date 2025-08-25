37104a7a253eb21b8b600f693dc6929715a56f5f
cuerojr <cannibal5033@gmail.com>
2025-08-25 13:03:52 -0300

    sitemaps dinamicos implementado

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,31 @@
 /** @type {import('next-sitemap').IConfig} */
 
+const SITE_URL = "https://epicarosario.com.ar";
+
 module.exports = {
-  siteUrl: 'https://epicarosario.com.ar',
+  siteUrl: SITE_URL,
   generateRobotsTxt: true,
-  changefreq: 'weekly',
+  changefreq: "weekly",
   priority: 0.7,
   sitemapSize: 5000,
+
+  additionalPaths: async (config) => {
+    // ⚡ Import dinámico porque next-sitemap corre en Node
+    const db = require("./lib/prisma"); // apunta al cliente Prisma JS
+
+    // Traer todas las publicaciones
+    const publicaciones = await db.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 15,
+    });
+
+    return publicaciones.map((p) => ({
+      loc: `/${p.slug}`,
+      lastmod: p.updatedAt.toISOString(),
+      changefreq: "weekly",
+      priority: 0.8,
+    }));
+  },
 };
